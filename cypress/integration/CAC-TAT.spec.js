aaf28b98a0c98cb23085df007e538951d755955c
Antonio Trindade <atrindade@umov.me>
2024-04-23 17:41:36 -0300

    Add new commands and new tests

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -129,4 +129,44 @@ describe('Central de Atendimento ao Cliente', function () {
             .should('not.be.checked')
     })
 
+    it('Seleciona um arquivo da pasta fixtures', function(){
+        cy.get('#file-upload')
+            .should("not.have.value")
+            .selectFile('./cypress/fixtures/example.json')
+            .should(function($input){
+                expect($input[0].files[0].name).to.equal('example.json')
+            })
+    })
+
+    it('Seleciona um arquivo simulando um drag-and-drop', function(){
+        cy.get('#file-upload')
+            .should("not.have.value")
+            .selectFile('./cypress/fixtures/example.json', {action: 'drag-drop'})
+            .should(function($input){
+                expect($input[0].files[0].name).to.equal('example.json')
+            })
+
+    })
+
+    it('Seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function(){
+        cy.fixture('example.json').as('sampleFile')
+        cy.get('#file-upload')
+            .selectFile('@sampleFile')
+            .should(function($input){
+                expect($input[0].files[0].name).to.equal('example.json')
+            })
+    })
+
+    it('Verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function(){
+        cy.get('#privacy a').should('have.attr', 'target', '_blank')
+    })
+
+    it('Acessa a página da política de privacidade removendo o target e então clicando no link', function(){
+        cy.get('#privacy a')
+            .invoke('removeAttr', 'target')
+            .click()
+        cy.contains('Não salvamos dados submetidos no formulário da aplicação CAC TAT.')
+            .should('be.visible')
+    })
+
 })
