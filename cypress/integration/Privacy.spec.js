991343fc8bd6b16d8d4d8794e0eb251b6287b4a0
Antonio Trindade <atrindade@umov.me>
2024-04-23 19:09:41 -0300

    Lodash times

--- cypress/integration/Privacy.spec.js
+++ cypress/integration/Privacy.spec.js
@@ -1,5 +1,7 @@
-it('testa a página da política de privacidade de forma independente', function(){
-    cy.visit('./src/privacy.html');
-    cy.contains('Não salvamos dados submetidos no formulário da aplicação CAC TAT.')
-            .should('be.visible')
+Cypress._.times(5, function(){
+    it('testa a página da política de privacidade de forma independente', function(){
+        cy.visit('./src/privacy.html');
+        cy.contains('Não salvamos dados submetidos no formulário da aplicação CAC TAT.')
+                .should('be.visible')
+    })
 })
